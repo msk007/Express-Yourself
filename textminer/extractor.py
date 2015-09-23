dd19085761d00809043bc0dc2bf19467c1a81d20
msk007 <krawiec515@gmail.com>
2015-09-22 22:08:03 -0400

    update

--- textminer/extractor.py
+++ textminer/extractor.py
@@ -3,5 +3,5 @@ import pytest
 import textminer
 
 
-def phone_numbers():
-    return re.findall(r'\(?\d{3}\W*\d{3}\-?\d{4}')
+def phone_numbers(text):
+    return re.findall(r'\(?\d{3}\W*\d{3}\-?\d{4}',text)
